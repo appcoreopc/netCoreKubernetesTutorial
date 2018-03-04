56be8462605c872e60a1fd52386d63de52fa65cb
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-03-05 07:38:20 +1300

    added command feature in slack

---
+++ ConnectCD-NetCoreWebApi/Controllers/CommandController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ConnectCD.NetCoreWebApi.Models;
+using ConnectCD.NetCoreWebApi.WordRank;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace ConnectCD.NetCoreWebApi.Controllers
+{
+    public class CommandController : Controller
+    {
+        private ILogger<CommandController> _logger;
+
+        public CommandController(ILogger<CommandController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        public async Task<IActionResult> TopWords([FromQuery] string command, string text, string user_name)
+        {            
+            if (!string.IsNullOrEmpty(text))
+            {
+                var topWords = await new WordRanker(new WordDataHandler(), _logger).GetTopWordsAsync(10);
+                return Ok(topWords);
+            }
+            
+            return Ok();
+        }
+    }
+}
---
+++ ConnectCD-NetCoreWebApi/Models/CommandModel.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ConnectCD.NetCoreWebApi.Models
+{
+    public class CommandModel
+    {
+        /// <summary>
+        ///  Authentication 
+        /// </summary>
+        public string Challenge { get; set; }
+
+        public string Token { get; set; }
+
+        public string Command { get; set; }
+        ///public string Token { get; set; }
+
+        public string Team_Id { get; set; }
+
+        public string Api_App_Id { get; set; }
+
+        public EventTypeInfo Event { get; set; }
+              
+        public string Type { get; set; }
+            
+        public string Event_Id { get; set; }
+        
+        public int Event_Time { get; set; }
+
+        public string[] Authed_Users { get; set; }
+        
+    } 
+    
+
+    
+}
